4f758034290afcf10f4a1de947a58589b810fc8e
Maxim Kirshin <meowto16@gmail.com>
2022-05-20 01:34:35 +0500

    Completed first section, hero 3d

--- src/index.js
+++ src/index.js
@@ -1,5 +1,28 @@
 import './styles/index.scss'
 
-document.addEventListener('DOMContentLoaded', () => {
-  console.log('DOM Ready!')
-})
+function main() {
+  initializeHero3D()
+}
+
+function initializeHero3D() {
+  const container = document.querySelector('.js-hero-3d-container')
+  const man = document.querySelector('.js-hero-3d-man')
+
+  container.addEventListener('mousemove', (e) => {
+    const MAX_TRANSLATE_MAN = 30
+
+    const { x, y, width, height } = container.getBoundingClientRect()
+    const top = e.clientY - y
+    const left = e.clientX - x
+
+    const translateX = left / width
+    const translateY = top / height
+
+    man.style.transform = `translate(
+      ${translateX * MAX_TRANSLATE_MAN}px, 
+      ${translateY * MAX_TRANSLATE_MAN}px)
+    `
+  })
+}
+
+document.addEventListener('DOMContentLoaded', main)
